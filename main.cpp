76b1e5dedbef2fff829ab49ec161eef5951f2dca
self care <49820562+ICANSTILLHEARYOU@users.noreply.github.com>
2021-12-13 16:12:49 +0300

    vJ

--- main.cpp
+++ main.cpp
@@ -1,8 +1,67 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
+int main()
+{
+	cout << "Enter your message: " << endl;
+	string text;
+	getline(cin, text);
+	cout << "Enter your key: " << endl;
+	string key;
+	getline(cin, key);
 
-int main() {
-    
-    return 0;
+	char *ntext = new char [text.size()-1];
+	char *nkey = new char [key.size()-1];
+
+	int k = 0;
+	for(int i = 0; i < text.size(); i++){
+		nkey[i] = key[k];
+		k++;
+		if (k == key.size()){
+			k = 0;
+		}
+	}
+	for (int i = 0; i < text.size(); i++){
+		if ((int(text[i])+int(nkey[i])) < 256){
+			ntext[i] = char((int(text[i])+int(nkey[i])));
+		}
+		else{
+			ntext[i] = char((int(text[i])+int(nkey[i])) - 256);
+		}
+		k = 0;
+	}
+
+	cout << "Encrypted message :" << endl;
+	cout << ntext << endl;
+	cout << "Enter the decryption key: " << endl;
+	string skey;
+	getline(cin, skey);
+
+	cout << "Decrypted message: " << endl;
+	for(int i = 0; i < text.size(); i++){
+		nkey[i] = skey[k];
+		k++;
+		if (k == key.size()){
+			k = 0;
+		}
+	}
+	for(int i = 0;i < text.size(); i++){
+		cout << nkey[i];
+	}
+	cout << endl;
+	for (int i = 0; i < text.size(); i++){
+		if ((int(text[i])-int(nkey[i])) < 0){
+			ntext[i] = char((int(ntext[i])-int(nkey[i])));
+		}
+		else{
+			ntext[i] = char((int(ntext[i])-int(nkey[i])) + 256);
+		}
+	}
+	for(int i = 0;i < text.size(); i++){
+		cout << ntext[i];
+	}
+	cout << endl;
+
+	return(0);
 }
