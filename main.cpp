018710a682be87bd610e60f3b47016b14ba8d805
self care <49820562+ICANSTILLHEARYOU@users.noreply.github.com>
2022-01-09 20:17:39 +0300

    Vigenere cipher working correctly

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 #include <string>
+
 using namespace std;
 
-int main()
-{
+int main() {
 	cout << "Enter your message: " << endl;
 	string text;
 	getline(cin, text);
@@ -11,23 +11,22 @@ int main()
 	string key;
 	getline(cin, key);
 
-	char *ntext = new char [text.size()-1];
-	char *nkey = new char [key.size()-1];
+	char *ntext = new char[text.size() - 1];
+	char *nkey = new char[key.size() - 1];
 
 	int k = 0;
-	for(int i = 0; i < text.size(); i++){
+	for (int i = 0; i < text.size(); i++) {
 		nkey[i] = key[k];
 		k++;
-		if (k == key.size()){
+		if (k == key.size()) {
 			k = 0;
 		}
 	}
-	for (int i = 0; i < text.size(); i++){
-		if ((int(text[i])+int(nkey[i])) < 256){
-			ntext[i] = char((int(text[i])+int(nkey[i])));
-		}
-		else{
-			ntext[i] = char((int(text[i])+int(nkey[i])) - 256);
+	for (int i = 0; i < text.size(); i++) {
+		if ((int(text[i]) + int(nkey[i])) < 256) {
+			ntext[i] = char((int(text[i]) + int(nkey[i])));
+		} else {
+			ntext[i] = char((int(text[i]) + int(nkey[i])) - 256);
 		}
 		k = 0;
 	}
@@ -39,29 +38,28 @@ int main()
 	getline(cin, skey);
 
 	cout << "Decrypted message: " << endl;
-	for(int i = 0; i < text.size(); i++){
+	for (int i = 0; i < text.size(); i++) {
 		nkey[i] = skey[k];
 		k++;
-		if (k == key.size()){
+		if (k == key.size()) {
 			k = 0;
 		}
 	}
-	for(int i = 0;i < text.size(); i++){
+	for (int i = 0; i < text.size(); i++) {
 		cout << nkey[i];
 	}
 	cout << endl;
-	for (int i = 0; i < text.size(); i++){
-		if ((int(text[i])-int(nkey[i])) < 0){
-			ntext[i] = char((int(ntext[i])-int(nkey[i])));
-		}
-		else{
-			ntext[i] = char((int(ntext[i])-int(nkey[i])) + 256);
+	for (int i = 0; i < text.size(); i++) {
+		if ((int(text[i]) - int(nkey[i])) < 0) {
+			ntext[i] = char((int(ntext[i]) - int(nkey[i])));
+		} else {
+			ntext[i] = char((int(ntext[i]) - int(nkey[i])) + 256);
 		}
 	}
-	for(int i = 0;i < text.size(); i++){
+	for (int i = 0; i < text.size(); i++) {
 		cout << ntext[i];
 	}
 	cout << endl;
 
-	return(0);
+	return (0);
 }
